ffb6cf155fd0f74c9b6aa62cf7797c4acfd09993
pranitha sai sri <pranithasai.e@gmail.com>
2022-01-13 17:42:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+ p = document.getElementById("principal").value;
+ r = document.getElementById("rate").value;
+ t = document.getElementById("years").value;
+ document.getElementById('pval').innerHTML = p;
+document.getElementById('rval').innerHTML = r;
+document.getElementById('aval').innerHTML = parseInt(p)+p*t*r/100;
+document.getElementById ('yval').innerHTML=new Date().getFullYear()+parseInt(t);
+document.getElementById("cont1").style.visibility="visible";
+       return
     
+}
+
+function change()
+{
+r = document.getElementById("rate").value;
+document.getElementById("rateVal").innerHTML=r;
+       return
 }
         
