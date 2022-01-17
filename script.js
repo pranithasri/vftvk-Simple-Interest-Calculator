009035ec5ccea0bf4bd1fbdb755c78b50d40376c
pranitha sai sri <pranithasai.e@gmail.com>
2022-01-17 08:38:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute()
  t = document.getElementById("years").value;
  document.getElementById('pval').innerHTML = p;
 document.getElementById('rval').innerHTML = r;
-document.getElementById('aval').innerHTML = parseInt(p)+p*t*r/100;
+document.getElementById('aval').innerHTML = p*t*r/100;
 document.getElementById ('yval').innerHTML=new Date().getFullYear()+parseInt(t);
 document.getElementById("cont1").style.visibility="visible";
        return
